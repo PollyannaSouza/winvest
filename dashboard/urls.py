56def8740a6eac2f098d40ae41b3ac4537388d57
Luís Felipe <luisfmds03@gmail.com>
2024-06-28 09:46:55 -0300

    Redirect

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from . import views
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('index/<int:user_id>/', views.index, name='index'),
-    path('login/', views.login_view, name='login')
+    path('login/', views.login_view, name='login'),
+    path('', lambda request: redirect('login'), name='home'),
 ]
